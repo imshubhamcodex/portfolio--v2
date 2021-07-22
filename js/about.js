25437f9a4407ade8b02f0da06114d0863f532870
imshubhamcodex <edwssm777@gmail.com>
2021-07-22 13:46:13 +0530

    about

--- js/about.js
+++ js/about.js
@@ -5,9 +5,8 @@ $(function () {
       {
         async leave() {
           const done = this.async();
-
           pageTransition();
-          await delay(800);
+          await delay(900);
           done();
         },
 
@@ -18,11 +17,10 @@ $(function () {
         async afterEnter() {
           let tl = gsap.timeline();
           tl.to("ul.transition li", {
-            duration: 0.5,
+            duration: 0.4,
             scaleY: 0,
             transformOrigin: "bottom left",
-            stagger: 0.05,
-            delay: 0.05,
+            stagger: 0.095,
           });
         },
 
@@ -36,21 +34,22 @@ $(function () {
 
 // function for page transition
 function pageTransition() {
+  if (
+    screen.width < 500 &&
+    document.getElementsByClassName("load-container").length > 0
+  ) {
+    document.getElementsByClassName("load-container")[0].style.display = "none";
+    document.getElementsByClassName("load-container_0")[0].style.display =
+      "none";
+  }
+
   let tl = gsap.timeline();
   tl.to("ul.transition li", {
-    duration: 0.5,
+    duration: 0.4,
     scaleY: 1,
     transformOrigin: "bottom left",
-    stagger: 0.06,
+    stagger: 0.09,
   });
-
-  // tl.to("ul.transition li", {
-  //   duration: 0.5,
-  //   scaleY: 0,
-  //   transformOrigin: "bottom left",
-  //   stagger: 0.05,
-  //   delay: 0.05,
-  // });
 }
 
 // delay function
@@ -86,4 +85,121 @@ function contentAnimation() {
   );
 
   // tl.to("#wrapper", { display: "block" });
+  tl.to(".mobile_nav_circle", { display: "none" });
+
+}
+
+function toggleNav() {
+  if (document.getElementById("bar_icon").classList.contains("fa-bars")) {
+    leftMove();
+  } else {
+    rightMove();
+  }
+}
+
+function leftMove() {
+  let tl = gsap.timeline();
+
+  document.getElementById("bar_icon").classList.remove("fa-bars");
+  document.getElementById("bar_icon").classList.add("fa-times");
+  document.getElementsByTagName("main")[0].style.display = "none";
+  document.getElementById("mobile_nav_items").style.display = "block";
+  document.getElementById("mobile_logo").style.display = "none";
+
+  tl.fromTo(
+    ".loading-screen_0",
+    {
+      duration: 1.2,
+      width: "100%",
+      left: "0%",
+      ease: "Expo.easeOut",
+    },
+    {
+      duration: 1.2,
+      width: "0%",
+      left: "0%",
+      ease: "Expo.easeOut",
+    }
+  );
+
+  tl.fromTo(
+    ".loading-screen",
+    {
+      duration: 1.1,
+      width: "100%",
+      left: "0%",
+      ease: "Expo.easeOut",
+    },
+    {
+      duration: 1.1,
+      width: "7%",
+      left: "0%",
+      ease: "Expo.easeOut",
+    },
+    "-=.85"
+  );
+
+  tl.to(".mobile_nav_circle", { display: "block" }, "-=2.1");
+
+  tl.fromTo(
+    ".icon_mob i",
+    { duration: 1, opacity: 0, x: -30, ease: "Expo.easeOut" },
+    { duration: 1, opacity: 1, x: 0, ease: "Back.easeOut", stagger: 0.1 },
+    "-=1"
+  );
+
+  tl.fromTo(
+    "#bar_icon",
+    { duration: 0.5, opacity: 0, y: -40, ease: "Back.easeOut" },
+    { duration: 0.5, opacity: 1, y: 0, ease: "Back.easeOut" },"-=0.8"
+  );
+
+}
+
+function rightMove() {
+  document.getElementById("bar_icon").classList.add("fa-bars");
+  document.getElementById("bar_icon").classList.remove("fa-times");
+  document.getElementsByTagName("main")[0].style.display = "block";
+  document.getElementById("mobile_nav_items").style.display = "none";
+  document.getElementById("mobile_logo").style.display = "block";
+
+  let tl = gsap.timeline();
+  tl.fromTo(
+    ".loading-screen_0",
+    {
+      duration: 1.2,
+      width: "100%",
+      left: "0%",
+      ease: "Expo.easeOut",
+    },
+    {
+      duration: 1.2,
+      width: "0%",
+      left: "100%",
+      ease: "Expo.easeOut",
+    }
+  );
+
+  tl.fromTo(
+    ".loading-screen",
+    {
+      duration: 1.1,
+      width: "100%",
+      left: "0%",
+      ease: "Expo.easeOut",
+    },
+    {
+      duration: 1.1,
+      width: "0%",
+      left: "100%",
+      ease: "Expo.easeOut",
+    },
+    "-=.85"
+  );
+
+  tl.fromTo(
+    "#bar_icon",
+    { duration: 0.5, opacity: 0, y: -40, ease: "Back.easeOut" },
+    { duration: 0.5, opacity: 1, y: 0, ease: "Back.easeOut" },"-=0.8"
+  );
 }
--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ $(function () {
         async leave() {
           const done = this.async();
           pageTransition();
-          await delay(800);
+          await delay(900);
           done();
         },
 
@@ -17,10 +17,10 @@ $(function () {
         async afterEnter() {
           let tl = gsap.timeline();
           tl.to("ul.transition li", {
-            duration: 0.5,
+            duration: 0.4,
             scaleY: 0,
             transformOrigin: "bottom left",
-            stagger: 0.06,
+            stagger: 0.095,
           });
         },
 
@@ -34,12 +34,21 @@ $(function () {
 
 // function for page transition
 function pageTransition() {
+  if (
+    screen.width < 500 &&
+    document.getElementsByClassName("load-container").length > 0
+  ) {
+    document.getElementsByClassName("load-container")[0].style.display = "none";
+    document.getElementsByClassName("load-container_0")[0].style.display =
+      "none";
+  }
+
   let tl = gsap.timeline();
   tl.to("ul.transition li", {
-    duration: 0.5,
+    duration: 0.4,
     scaleY: 1,
     transformOrigin: "bottom left",
-    stagger: 0.06,
+    stagger: 0.09,
   });
 }
 
@@ -76,6 +85,7 @@ function contentAnimation() {
   );
 
   tl.to("#wrapper", { display: "block" });
+  tl.to(".mobile_nav_circle", { display: "none" });
 
   tl.fromTo(
     "#social div ul li",
@@ -95,7 +105,7 @@ function contentAnimation() {
     "#bottom_text p",
     { duration: 0.7, opacity: 0, x: -100, ease: "Expo.easeOut" },
     { duration: 0.7, opacity: 1, x: 0, stagger: 0.1, ease: "Expo.easeOut" },
-    "-=0.3"
+    "-=0.6"
   );
 }
 
@@ -108,10 +118,14 @@ function toggleNav() {
 }
 
 function leftMove() {
+  let tl = gsap.timeline();
+
   document.getElementById("bar_icon").classList.remove("fa-bars");
   document.getElementById("bar_icon").classList.add("fa-times");
+  document.getElementsByTagName("main")[0].style.display = "none";
+  document.getElementById("mobile_nav_items").style.display = "block";
+  document.getElementById("mobile_logo").style.display = "none";
 
-  let tl = gsap.timeline();
   tl.fromTo(
     ".loading-screen_0",
     {
@@ -144,11 +158,30 @@ function leftMove() {
     },
     "-=.85"
   );
+
+  tl.to(".mobile_nav_circle", { display: "block" }, "-=2.1");
+
+  tl.fromTo(
+    ".icon_mob i",
+    { duration: 1, opacity: 0, x: -30, ease: "Expo.easeOut" },
+    { duration: 1, opacity: 1, x: 0, ease: "Back.easeOut", stagger: 0.1 },
+    "-=1"
+  );
+
+  tl.fromTo(
+    "#bar_icon",
+    { duration: 0.5, opacity: 0, y: -40, ease: "Back.easeOut" },
+    { duration: 0.5, opacity: 1, y: 0, ease: "Back.easeOut" },"-=0.8"
+  );
+
 }
 
 function rightMove() {
   document.getElementById("bar_icon").classList.add("fa-bars");
   document.getElementById("bar_icon").classList.remove("fa-times");
+  document.getElementsByTagName("main")[0].style.display = "block";
+  document.getElementById("mobile_nav_items").style.display = "none";
+  document.getElementById("mobile_logo").style.display = "block";
 
   let tl = gsap.timeline();
   tl.fromTo(
@@ -182,5 +215,11 @@ function rightMove() {
       ease: "Expo.easeOut",
     },
     "-=.85"
+  );
+
+  tl.fromTo(
+    "#bar_icon",
+    { duration: 0.5, opacity: 0, y: -40, ease: "Back.easeOut" },
+    { duration: 0.5, opacity: 1, y: 0, ease: "Back.easeOut" },"-=0.8"
   );
 }
